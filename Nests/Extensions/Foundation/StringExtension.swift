f38868be8707c9e965f16d8a145c0c050e3ac386
Jiang Liang <bluedream5161@gmail.com>
2019-12-17 21:01:39 +0800

    增加了取子字符串位置的方法

--- Nests/Extensions/Foundation/StringExtension.swift
+++ Nests/Extensions/Foundation/StringExtension.swift
@@ -18,7 +18,6 @@ extension String {
         if from >= to {
             return nil
         }
-    
         
         var end: Int = Int(to)
         if to > self.count {
@@ -30,4 +29,23 @@ extension String {
         return String(self[startIndex..<endIndex])
     }
     
+    public func ranges(ofString: String) -> [Range<String.Index>]? {
+        if ofString.isEmpty || self.isEmpty {
+            return nil
+        }
+        
+        guard let sr = range(of: self) else {
+            return nil
+        }
+        
+        var ranges = [Range<String.Index>]()
+        var searchRange = sr
+      
+        while let range = range(of: ofString, options: .regularExpression, range: searchRange, locale: nil) {
+            ranges.append(range)
+            searchRange = Range(uncheckedBounds: (range.upperBound, self.endIndex))
+        }
+        
+        return ranges
+    }
 }
